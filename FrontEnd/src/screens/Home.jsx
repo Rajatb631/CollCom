b2525070afc1b4b8f21aa4a0c85c0e927de4f5eb
vinayak-repositories <vinayak.gm.acc@gmail.com>
2025-01-24 14:57:01 +0530

    Footer phase 1

---
+++ FrontEnd/src/screens/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import '../css/Navbar.css'
+import Footer from '../components/Footer'
+import '../css/Footer.css'
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home
