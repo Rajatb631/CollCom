60fee527ef244b82b5474aff0ebd00c623d35eeb
vinayak-repositories <vinayak.gm.acc@gmail.com>
2025-01-28 23:46:33 +0530

    Home page fixing

--- FrontEnd/src/screens/main.jsx
+++ FrontEnd/src/screens/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import CyberSanskar from './CyberSanskar'
 import '../css/CyberSanskar.css'
+import Home from './Home'
+import '../css/Home.css'
+import AboutUs from './AboutUs'
+import '../css/BannerCompo.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CyberSanskar/>
+    {/* <CyberSanskar/> */}
+    <Home/>
+    {/* <AboutUs/> */}
   </StrictMode>,
 )
